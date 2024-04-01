f49fd8efc757839b10779ae09fd11a18ee470ca2
val <v@lentin.fr>
2024-04-01 16:10:07 +0200

    Created a script to dynamically import Python files from a specified directory.

---
+++ agentix/magicimport.py
@@ -0,0 +1,36 @@
+import glob
+import os
+import sys
+from rich import print
+
+
+_DEBUG = os.getenv('AGENTFLOW_DEBUG')
+
+def dynamic_import(directory_name):
+    """
+    Dynamically imports all Python files from the specified directory under the current working directory.
+    """
+
+    parent = os.path.dirname
+    target_dir = os.getcwd()
+
+    if target_dir not in sys.path:
+        sys.path.append(target_dir)
+    
+    target_files = glob.glob(target_dir + f'/**/**/{directory_name}/**/*.py', recursive=True)
+    
+
+    
+    for file_path in target_files:
+        _DEBUG and print(f"dynamic import of[orange i] `{file_path}`")
+        # Extract module name from file path, excluding the directory part
+        relative_path = os.path.relpath(file_path, target_dir)
+        sys.path.append(relative_path)
+        module_name = os.path.splitext(relative_path)[0].replace(os.sep, '.')
+        
+        # Import module
+        if module_name not in sys.modules:
+            __import__(module_name)
+            
+
+
