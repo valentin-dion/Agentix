5042725cd039cef17b130a8a552d9776fededf1b
val <v@lentin.fr>
2024-04-15 14:49:27 +0200

    Updated class name and added a new file property for the endpoint docstring.

--- agentix/wrappers/endpoint.py
+++ agentix/wrappers/endpoint.py
@@ -5,16 +5,16 @@ from .func_wrapper import FuncWrapper, file_property
 from .instances_store import InstancesStore
 
 
-class Func(FuncWrapper, metaclass=InstancesStore):
+class Endpoint(FuncWrapper, metaclass=InstancesStore):
     
     code = file_property('code.py')
+    docstring = file_property('docstring.md')
     
     def __init__(self, name: str, func: callable):
         super().__init__(name, func) 
         cwd = os.getcwd()
-        self.dir_path = os.path.join(cwd, "funcs", name)
+        self.dir_path = os.path.join(cwd, "endpoints", name)
         os.makedirs(self.dir_path, exist_ok=True)
 
  
 
-func = Func.register 
